fec1f583e07be07fed297c4feae7de7989ab385e
jfy0o0 <406613966@qq.com>
2023-07-22 10:17:25 +0800

    修复 gshost 前置reader已经读走bufio

--- net/gshost/shared.go
+++ net/gshost/shared.go
@@ -15,27 +15,29 @@ const (
 type SharedConn struct {
 	sync.Mutex
 	net.Conn
-	gsHostBuf *bytes.Buffer // all of the initial data that has to be read in order to vhost a connection is saved here
+	gsHostBuf  *bytes.Buffer // all of the initial data that has to be read in order to vhost a connection is saved here
+	connReader *bufio.Reader
 }
 
 func NewShared(conn net.Conn, f ...func(tempReader *bufio.Reader)) (*SharedConn, io.Reader) {
 	c := &SharedConn{
-		Conn:      conn,
-		gsHostBuf: bytes.NewBuffer(make([]byte, 0, initGsHostBufSize)),
+		Conn:       conn,
+		gsHostBuf:  bytes.NewBuffer(make([]byte, 0, initGsHostBufSize)),
+		connReader: bufio.NewReader(conn),
 	}
-	reader := bufio.NewReader(conn)
+	//reader := bufio.NewReader(conn)
 
 	if len(f) > 0 {
-		f[0](reader)
+		f[0](c.connReader)
 	}
-	return c, io.TeeReader(reader, c.gsHostBuf)
+	return c, io.TeeReader(c.connReader, c.gsHostBuf)
 }
 
 func (c *SharedConn) Read(p []byte) (n int, err error) {
 	c.Lock()
 	if c.gsHostBuf == nil {
 		c.Unlock()
-		return c.Conn.Read(p)
+		return c.connReader.Read(p)
 	}
 	n, err = c.gsHostBuf.Read(p)
 
@@ -47,7 +49,7 @@ func (c *SharedConn) Read(p []byte) (n int, err error) {
 
 		// continue reading from the connection
 		var n2 int
-		n2, err = c.Conn.Read(p[n:])
+		n2, err = c.connReader.Read(p[n:])
 
 		// update total read
 		n += n2
