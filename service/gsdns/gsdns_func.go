7b256fbdce53b7a2dc2aad39358cc87225b2a10a
jfy0o0 <406613966@qq.com>
2023-08-10 18:43:59 +0800

    gsdns GetDnsUserRequest srcip

--- service/gsdns/gsdns_func.go
+++ service/gsdns/gsdns_func.go
@@ -3,15 +3,19 @@ package gsdns
 import (
 	"bytes"
 	"encoding/binary"
+	"github.com/jfy0o0/goStealer/net/gsipv4"
 	"golang.org/x/net/dns/dnsmessage"
 	"net"
 )
 
 func GetDnsUserRequest[T any](src *net.UDPAddr, buf []byte, n int, value T) ([]byte, int, *UserDnsRequest[T]) {
+	srcIPString, _, _ := net.SplitHostPort(src.String())
+
 	var (
-		srcIP         uint32
+		srcIP         uint32 = gsipv4.Ip2long(srcIPString)
 		springBoardIP uint32
 	)
+
 	if n > userProtocolHeaderLen+8 {
 		if bytes.Equal(buf[:userProtocolHeaderLen], userProtocolHeader) {
 			srcIP = binary.BigEndian.Uint32(buf[userProtocolHeaderLen : userProtocolHeaderLen+4])
