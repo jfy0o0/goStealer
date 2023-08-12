b7675063aacf5a8b4d89ac921e6cc639348b57de
jfy0o0 <406613966@qq.com>
2023-08-12 14:07:21 +0800

    gsdns GetDnsIPs

---
+++ service/gsdns/gsdns_public.go
@@ -0,0 +1,11 @@
+package gsdns
+
+var GsDnsPublicIPs = []string{
+	"119.29.29.29", //腾讯云旗下的公共 DNS 服务
+	"223.5.5.5",    //AliDNS 阿里公共 DNS
+	"223.6.6.6",
+	"114.114.114.114",
+	"114.114.115.115",
+	"180.76.76.76", //百度 BaiduDNS
+	"8.8.8.8",      //Google Public DNS
+}
